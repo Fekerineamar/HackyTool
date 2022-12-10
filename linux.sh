e1f419ee5c36b00bb19acfb604ed17a5084bc60d
Cody4code <69634399+Fekerineamar@users.noreply.github.com>
2022-12-10 21:32:46 +0100

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,26 +1,31 @@
-#!/bin/bash
+!/usr/bin/env bash
 
-sudo apt update & sudo apt upgrade -y &
-sudo apt install python3 -y &
-sudo apt install nmap -y &
-wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz & 
-mkdir ~/.go &
-echo "GOPATH=$HOME/.go" >> ~/.bashrc &
-echo "export GOPATH" >> ~/.bashrc &
-echo "PATH=\$PATH:\$GOPATH/bin # Add GOPATH/bin to PATH for scripting" >> ~/.bashrc source ~/.bashrc &
-go install -v github.com/tomnomnom/waybackurls@latest &
-go install -v github.com/OWASP/Amass/v3/...@master &
-go install -v github.com/lukasikic/subzy@latest &
-go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest &
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest &
-go install -v github.com/Emoe/kxss@latest &
-go install -v github.com/tomnomnom/gf@latest &
-pip install dirsearch &
-pip install uro & 
-pip install arjun &
-cd ~/Desktop &
-git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev &
-git clone https://github.com/devanshbatham/ParamSpider &
-cd ParamSpider &
+Green='\033[0;32m'
+NC='\033[0m' # No Color
+echo "{GREEN}Starting Tool!!{NC}"
+sleep 2
+sudo apt update && sudo apt upgrade -y &&
+sudo apt install python3 -y &&
+sudo apt install nmap -y &&
+wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz && 
+mkdir ~/.go &&
+echo "GOPATH=$HOME/.go" >> ~/.bashrc &&
+echo "export GOPATH" >> ~/.bashrc &&
+echo "PATH=\$PATH:\$GOPATH/bin # Add GOPATH/bin to PATH for scripting" >> ~/.bashrc source ~/.bashrc &&
+sudo rm go1.19.4.linux-amd64.tar.gz &&
+go install -v github.com/tomnomnom/waybackurls@latest &&
+go install -v github.com/OWASP/Amass/v3/...@master &&
+go install -v github.com/lukasikic/subzy@latest &&
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest &&
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest &&
+go install -v github.com/Emoe/kxss@latest &&
+go install -v github.com/tomnomnom/gf@latest &&
+pip install dirsearch &&
+pip install uro && 
+pip install arjun &&
+cd ~/Desktop &&
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev &&
+git clone https://github.com/devanshbatham/ParamSpider &&
+cd ParamSpider &&
 pip3 install -r requirements.txt
 echo "done :)!"
