9c7402cfac9fb518cf859359ac4459cd99f733c1
Cody4code <69634399+Fekerineamar@users.noreply.github.com>
2024-04-16 14:01:53 +0100

    Update linux.sh

--- tools/linux.sh
+++ tools/linux.sh
@@ -7,7 +7,7 @@ sudo apt install -y python3 &&
 sudo apt install -y python3-pip &&
 sudo apt install -y nmap &&
 sudo apt install -y libpcap-dev &&
-wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz && 
+wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz && 
 sudo rm -rf /usr/local/go && 
 sudo tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz &&
 mkdir -p ~/.go &&
